508159cc7846661f11c822c4ea5e806983bd4adb
VladTrofymyshyn <vladtrofymyshyn@gmail.com>
2023-12-21 17:01:48 +0200

    dssd

--- main.py
+++ main.py
@@ -1,142 +1,27 @@
-[17: 48] Трофимишин
-Владислав
-Тарасович
-print(10 / 0)
-
-except ZeroDivisionError as e:
-
-print(f"Ділити на нуль не можна! {e}")
-
-finally:
-
-print("Далі якийсь код")
-
-try:
-
-    number = int(input("Введіть число:"))
-
-except ValueError as e:
-
-    print(e)
-
-
-# підняття винятків
-
-# text = input("Введіть текст! ")
-
-# if type(text) != int:
-
-#     raise TypeError(f"Ми не працюємо з цим {type(text)} типом")
-
-# генерація власних винятків
-
-class BuildingError(Exception):
-
-    def __str__(self):
-        return "Не вистачає матеріалів! Не можливо побудувати"
-
-
-def check_materials(amont_materials, limit_materials):
-    if amont_materials > limit_materials:
-
-        print("Матеріалу достатньо!")
-
-    else:
-
-        raise BuildingError
-
-
-my_materials = 400
-
-check_materials(my_materials, 300)  # 300 мінімальна кількість
-
-
-class InvalidEmailError(Exception):
-
-    def __init__(self, email):
-        self.email = email
-
-
-def send_email(email, message):
-    if "@" not in email:
-        raise InvalidEmailError("Invalid Email " + email)
-
-
-try:
-
-    send_email("example_email.com", "Hello!")
-
-except InvalidEmailError as e:
-
-    print(e)
-[17: 48]Трофимишин
-Владислав
-Тарасович
-print(10 / 0)
-
-except ZeroDivisionError as e:
-
-print(f"Ділити на нуль не можна! {e}")
-
-finally:
-
-print("Далі якийсь код")
-
-try:
-
-    number = int(input("Введіть число:"))
-
-except ValueError as e:
-
-    print(e)
-
-
-# підняття винятків
-
-# text = input("Введіть текст! ")
-
-# if type(text) != int:
-
-#     raise TypeError(f"Ми не працюємо з цим {type(text)} типом")
-
-# генерація власних винятків
-
-class BuildingError(Exception):
-
-    def __str__(self):
-        return "Не вистачає матеріалів! Не можливо побудувати"
-
-
-def check_materials(amont_materials, limit_materials):
-    if amont_materials > limit_materials:
-
-        print("Матеріалу достатньо!")
-
-    else:
-
-        raise BuildingError
-
-
-my_materials = 400
-
-check_materials(my_materials, 300)  # 300 мінімальна кількість
-
-
-class InvalidEmailError(Exception):
-
-    def __init__(self, email):
-        self.email = email
-
-
-def send_email(email, message):
-    if "@" not in email:
-        raise InvalidEmailError("Invalid Email " + email)
-
-
-try:
-
-    send_email("example_email.com", "Hello!")
-
-except InvalidEmailError as e:
-
-    print(e)
+lst = ["one", 2, 3]
+index = lst[0]
+print(index)
+# for elem in lst:
+#     print(elem)
+iterator = iter(lst)
+print(iterator)
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+class MyIterator:
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.index >= len(self.data):
+            raise StopIteration
+        value = self.data[self.index]
+        self.index += 1
+        return value
+
+my_list = [1, 6, 3, 9, 5]
+my_iterator = MyIterator(my_list)
+print(my_iterator.__iter__())
+print(*my_iterator)
